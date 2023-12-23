8d54a58c3683418ec63cd7f64290ecaa2324233f
Tig Kindel <tig@users.noreply.github.com>
2023-12-23 14:12:21 -0700

    Initial commit

--- UICatalog/Scenarios/Generic.cs
+++ UICatalog/Scenarios/Generic.cs
@@ -21,6 +21,17 @@ public sealed class MyScenario : Scenario
         button.Accept += (s, e) => MessageBox.ErrorQuery ("Error", "You pressed the button!", "Ok");
         appWindow.Add (button);
 
+        var bar = new Bar()
+        {
+
+        };
+        var barITem = new BarItem() { Text = "Quit", AutoSize = true };
+        barITem.KeyBindings.Add(Key.Q.WithCtrl, KeyBindingScope.Application, Command.QuitToplevel);
+        bar.Add(barITem);
+        bar.Add(new Label() { Text = "Item2" });
+        bar.Add(new Button() { Text = "_Press Me" });
+        appWindow.Add(bar);
+
         // Run - Start the application.
         Application.Run (appWindow);
         appWindow.Dispose ();
